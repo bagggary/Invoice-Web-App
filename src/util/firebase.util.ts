9cf012f4019dc2860675232052b52f8665ea1255
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-05-14 14:45:01 +0300

    update firebase auth provider

--- src/util/firebase.util.ts
+++ src/util/firebase.util.ts
@@ -18,17 +18,15 @@ import { getDatabase } from "firebase/database";
 // };
 const firebaseConfig = {
   apiKey: import.meta.env.REACT_APP_API_KEY,
-  authDomain:import.meta.env.REACT_APP_AUTH_DOMAIN,
-  projectId:import.meta.env.REACT_APP_PROJECT_ID,
-  storageBucket:import.meta.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID ,
+  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: import.meta.env.REACT_APP_ID,
-  databaseURL : import.meta.env.REACT_APP_DATABASE_URL
+  databaseURL: import.meta.env.REACT_APP_DATABASE_URL,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const  Auth = getAuth(app);
-export const db = getDatabase() ;
-
-
+export const Auth = getAuth(app);
+export const db = getDatabase();
