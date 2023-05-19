2f0f39304452c3110e8714b427d6b9e14cf23374
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-05-19 21:58:35 +0300

    auth functions to handle SignIn and SignUp

--- src/util/firebase.util.ts
+++ src/util/firebase.util.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { getDatabase, onValue, ref, set } from "firebase/database";
+import Data from "../assets/Data.json";
 
 const firebaseConfig = {
   apiKey: import.meta.env.REACT_APP_API_KEY,
@@ -16,3 +17,19 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const Auth = getAuth(app);
 export const db = getDatabase();
+
+export const createDocumentFromUserAuth = async (userId: string | null) => {
+  const userDocRef = ref(db, `user/${userId}`);
+  onValue(userDocRef, async (snapshot) => {
+    console.log(snapshot.exists());
+    if (!snapshot.exists()) {
+      try {
+        await set(userDocRef, { Data });
+      } catch (error) {
+        console.error("user Created Error ", error);
+      }
+    }
+    const userData = snapshot.val();
+    console.log(userData);
+  });
+};
