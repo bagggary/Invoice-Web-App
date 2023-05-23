524a50d31ab1b6d47fc78e46aad1467acbd7a640
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-05-24 00:02:30 +0300

    handle async function for invoice data

---
+++ src/store/invoice/invoice.saga.ts
@@ -0,0 +1,24 @@
+import { call, put, takeLatest, all } from "redux-saga/effects";
+import { INVOICES_ACTION_TYPES } from "./invoice.types";
+import { getInvoicesAndDocument } from "../../util/firebase.util";
+import { fetchInvoicesFailed, fetchInvoicesSuccess } from "./invoice.action";
+
+export function* fetchInvoicesAsync() {
+  try {
+    const invoicesArray = yield call(getInvoicesAndDocument);
+    yield put(fetchInvoicesSuccess(invoicesArray));
+  } catch (error: Error | any) {
+    yield put(fetchInvoicesFailed(error));
+  }
+}
+
+export function* onFetchInvoices() {
+  yield takeLatest(
+    INVOICES_ACTION_TYPES.FETCH_INVOICES_START,
+    fetchInvoicesAsync
+  );
+}
+
+export function* invoicesSaga() {
+  yield all([call(onFetchInvoices)]);
+}
