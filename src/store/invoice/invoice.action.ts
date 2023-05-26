6b9d6968e0b13606d4c8d17f9999be36b8fca2cb
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-05-26 23:27:39 +0300

    init multiple actions for state status

--- src/store/invoice/invoice.action.ts
+++ src/store/invoice/invoice.action.ts
@@ -1,5 +1,20 @@
-import { IVOICES_ACTION_TYPES } from "./invoice.types";
+import { INVOICES_ACTION_TYPES } from "./invoice.types";
 
-export const setInvoicesMap = (invioces: any) => {
-  return { type: IVOICES_ACTION_TYPES.SET_INVOICES_DATA, payload: invioces };
+export const clearInvoices = () => {
+  return { type: INVOICES_ACTION_TYPES.CLEAR_INVOICES };
+};
+
+export const fetchInvoicesStart = () => {
+  return { type: INVOICES_ACTION_TYPES.FETCH_INVOICES_START };
+};
+
+export const fetchInvoicesSuccess = (invoices) => {
+  return {
+    type: INVOICES_ACTION_TYPES.FETCH_INVOICES_SUCCESS,
+    payload: invoices,
+  };
+};
+
+export const fetchInvoicesFailed = (error: Error) => {
+  return { type: INVOICES_ACTION_TYPES.FETCH_INVOICES_FAILED, payload: error };
 };
