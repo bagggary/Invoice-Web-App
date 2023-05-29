2e50d6e4b9aea803032a1e3b54546da6f8988640
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-05-29 23:49:04 +0300

    add onAuthStateChange observer pattern to track user sub and unsub when uer log out

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
