751e2ee253798484bac3d86d376f8986971f2cdc
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-01 22:48:18 +0300

    added routes to and rules if auth not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import Authentication from "./components/Authentication/Authentication.component";
+import Authentication from "./routes/Authentication/Authentication.component";
 import "./index.css";
 import {
   createDocumentFromUserAuth,
@@ -7,20 +7,27 @@ import {
 } from "./util/firebase.util";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./store/user/user.action";
+import { Route, Routes } from "react-router-dom";
+import Main from "./routes/Main/Main.component";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
-        console.log(user);
         createDocumentFromUserAuth(user);
       }
       dispatch(setCurrentUser(user));
     });
     return unsubscribe;
   }, []);
-  return <Authentication />;
+
+  return (
+    <Routes>
+      <Route path="/" element={<Authentication />} />
+      <Route path="/home" element={<Main />} />
+    </Routes>
+  );
 }
 
 export default App;
