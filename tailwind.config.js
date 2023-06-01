6ac3dce17b956fda1b83668f6ac0445e7b275e65
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-01 22:46:44 +0300

    fonts and util config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7c5dfa",
+        secondry: "#9277ff",
+        red: "#EC5757",
+        "red-light": "#FF9797",
+        "gray-light": "#DFE3FA",
+        "gray-dark": "#888EBO",
+        "blue-dark": "#1E2139",
+        "blue-light": "#252945",
+        "light-BG": "#F8f8fb",
+        black: "#141625",
+        "black-1": "#0C0E16",
+        torko: "#7E88C3",
+      },
+    },
   },
   plugins: [],
 };
