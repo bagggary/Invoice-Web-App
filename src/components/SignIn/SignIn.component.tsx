07de7611d1b4fd7111bd3bf5b48f32583c251c0e
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-03 23:44:13 +0300

    sign in authentication config and side nav styles

--- src/components/SignIn/SignIn.component.tsx
+++ src/components/SignIn/SignIn.component.tsx
@@ -1,15 +1,15 @@
 // import Logo from '../../assets/logo.svg'
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { Auth, createDocumentFromUserAuth, db } from "../../util/firebase.util";
+import { Auth, createDocumentFromUserAuth } from "../../util/firebase.util";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "../../store/user/user.action";
-import { onValue, ref } from "firebase/database";
-import { setInvoicesMap } from "../../store/invoice/invoice.action";
-// import { setInvoicesMap } from "../../store/invoice/invoice.action";
 // import { onValue, ref } from "firebase/database";
+import { fetchInvoicesStart } from "../../store/invoice/invoice.action";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   type SignInTypes = {
     email: string;
@@ -41,28 +41,17 @@ const SignIn = () => {
     try {
       const { user } = await signInWithEmailAndPassword(Auth, email, password);
       dispatch(setCurrentUser(user));
-      resetFormFields();
-      // const userRef = ref(db, `user/${uid}`);
-      // onValue(userRef, (snapshot) => {
-      //   const userData = snapshot.val();
-      //   dispatch(setInvoicesMap(userData));
-      // });
-      const { uid } = user;
+      navigate("/home");
       await createDocumentFromUserAuth(user);
-      const userRef = ref(db, `user/${uid}`);
-      onValue(userRef, (snapshot) => {
-        const userData = snapshot.val();
-        dispatch(setInvoicesMap(userData));
-      });
-      // const userData = await createDocumentFromUserAuth(uid);
-      // dispatch(setInvoicesMap());
+      dispatch(fetchInvoicesStart());
     } catch (error) {
       console.error(error);
     }
+    resetFormFields();
   };
 
   return (
-    <div className="flex  flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a
         href="#"
         className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
@@ -116,27 +105,12 @@ const SignIn = () => {
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
-            {/* <div>
-                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
-                    </div> */}
-            {/* <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required={true} />
-                        </div>
-                        <div className="ml-3 text-sm">
-                          <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                        </div>
-                    </div> */}
             <button
               type="submit"
               className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Sign In
             </button>
-            {/* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                    </p> */}
           </form>
         </div>
       </div>
@@ -144,3 +118,26 @@ const SignIn = () => {
   );
 };
 export default SignIn;
+
+{
+  /* <div>
+                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
+                    </div> */
+}
+{
+  /* <div className="flex items-start">
+                        <div className="flex items-center h-5">
+                          <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required={true} />
+                        </div>
+                        <div className="ml-3 text-sm">
+                          <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
+                        </div>
+                    </div> */
+}
+
+{
+  /* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                    </p> */
+}
--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -3,8 +3,9 @@ import darkSwitch from "../../assets/icon-moon.svg";
 import logOut from "../../assets/log-out.svg";
 
 function Sidebar() {
+  //class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
   return (
-    <aside className=" md:fixed bg-[#373B53] left-0 top-0 md:h-full h-20 md:w-[103px] w-full  md:rounded-r-[20px] flex md:flex-col justify-between">
+    <aside className=" md:fixed bg-[#373B53] md:h-screen left-0 top-0  h-20 md:w-[103px] w-full  md:rounded-r-[20px] flex md:flex-col justify-between">
       <div className="md:w-full md:h-[103px] h-20 w-20 bg-primary flex justify-center items-center rounded-r-[20px] relative overflow-hidden">
         <div className="absolute w-full h-full top-[50%] bg-secondry rounded-l-[20px] "></div>
         <img
@@ -20,7 +21,7 @@ function Sidebar() {
         <div className=" group flex justify-center items-center md:py-6 md:border-t  border-r-0 border-[#494E6E] px-8 border-l md:relative">
           <img src={logOut} alt="logOut" className=" h-6 cursor-pointer" />
           <div className="absolute -right-20 z-20 top-4 text-white bg-[#373B53] p-2 rounded-lg invisible group-hover:visible">
-            logOut
+            Sign out
           </div>
         </div>
       </div>
