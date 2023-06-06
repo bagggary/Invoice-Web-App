3e890e9d44889b38cb796c5c9dcb1118fe033fae
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-06 15:59:02 +0300

    applied dark styles on invoices component

--- src/components/invoice/invoice.component.tsx
+++ src/components/invoice/invoice.component.tsx
@@ -1,4 +1,5 @@
 import rightarrow from "../../assets/icon-arrow-right.svg";
+import Status from "../status/status.component";
 
 type InvoicesProps = {
   paymentDue: string;
@@ -21,25 +22,35 @@ const Invoice: React.FC<InvoiceProps> = ({ data }) => {
   });
 
   return (
-    <div className="border border-transparent hover:border-primary flex  justify-around items-center pl-8 pr-6 py-6 rounded-lg cursor-pointer">
-      <div className=" text-sm font-bold ">
-        <span className=" text-torko">#</span>
-        {id}
+    <div className=" border dark:bg-blue-dark hover:border-solid hover:shadow-none border-transparent duration-200 ease-linear hover:border-primary flex  justify-between items-center pl-8 pr-6 py-6 rounded-lg cursor-pointer">
+      <div className="flex justify-between items-center mr-[2em]">
+        <div className=" text-sm font-bold mr-[1.75rem] dark:text-white text-black-1 ">
+          <span className=" text-torko">#</span>
+          {id}
+        </div>
+        <div className="text-sm text-torko font-medium text-center ml-auto dark:text-gray-light">
+          <span className=" text-dark-gray dark:text-gray-light">Due</span>{" "}
+          {formatedDate}
+        </div>
       </div>
-      <div className="text-sm text-torko font-medium text-center">
-        <span className=" text-dark-gray">Due</span> {formatedDate}
+      <div className="flex items-center w-[60%] ">
+        <div className="flex justify-between items-center w-full">
+          <p className="text-sm inline-block font-medium text-[#858BB2] dark:text-white">
+            {clientName}
+          </p>
+          <div className="font-bold text-black-1 text-lg dark:text-white">
+            £{" "}
+            {total.toLocaleString("en-US", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+          </div>
+        </div>
+        <div className="flex items-center">
+          <Status status={status} />
+          <img src={rightarrow} alt="rightarrow" className=" ml-5" />
+        </div>
       </div>
-      <p className="text-sm inline-block font-medium text-[#858BB2]">
-        {clientName}
-      </p>
-      <div className="font-bold text-black-1 text-lg ">
-        £ {total.toLocaleString("en-US")}
-      </div>
-      <div className="bg-slate-400 text-center flex items-center font-bold text-sm  w-[104px] h-10  justify-center text-[#33D69F] gap-2">
-        <div className=" w-2 h-2 bg-[#33D69F] rounded-full"></div>
-        {status}
-      </div>
-      <img src={rightarrow} alt="rightarrow" className=" ml-5" />
     </div>
   );
 };
