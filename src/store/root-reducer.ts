c6fe296586f800ae8ea621b71f28042b65500075
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-11 02:51:37 +0300

    added dark switch reducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
 import { invoiceReducer } from "./invoice/invoice.reducer";
+import { themeReducer } from "./switch/switch.reducer";
 
 export const rootReducer = combineReducers({
   user: userReducer,
   invoices: invoiceReducer,
+  theme: themeReducer,
 });
