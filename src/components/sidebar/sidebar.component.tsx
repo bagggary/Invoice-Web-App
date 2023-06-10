360b7ecafe5f4940363034896874c84e005f889c
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-11 02:52:25 +0300

    dark and light switch

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,13 +1,32 @@
 import logo from "../../assets/logo.svg";
 import darkSwitch from "../../assets/icon-moon.svg";
+import lightSwitch from "../../assets/icon-sun.svg";
 import logOut from "../../assets/log-out.svg";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectToggleSwitch } from "../../store/switch/switch.selector";
+import { setDarkTheme } from "../../store/switch/switch.action";
+import { useEffect } from "react";
 
 function Sidebar() {
+  const switchToggler = useSelector(selectToggleSwitch);
+  const dispatch = useDispatch();
+
+  const toggleSwitch = () => {
+    dispatch(setDarkTheme(!switchToggler));
+  };
+
+  useEffect(() => {
+    document.body.setAttribute(
+      "data-theme",
+      `${switchToggler ? "dark" : "light"}`
+    );
+  }, [switchToggler]);
+
   //class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
   return (
     <>
-      <aside className=" md:fixed bg-[#373B53] md:h-screen left-0 top-0  h-20 md:w-[103px] w-full  md:rounded-r-[20px] flex md:flex-col justify-between">
+      <aside className=" fixed bg-[#373B53] md:h-screen md:left-0 top-0  h-20 md:w-[103px] w-full  md:rounded-r-[20px] flex md:flex-col justify-between">
         <div className="md:w-full md:h-[103px] h-20 w-20 bg-primary flex justify-center items-center rounded-r-[20px] relative overflow-hidden">
           <div className="absolute w-full h-full top-[50%] bg-secondry rounded-l-[20px] "></div>
           <img
@@ -17,8 +36,15 @@ function Sidebar() {
           />
         </div>
         <div className="flex md:flex-col">
-          <div className="flex justify-center items-center md:pb-8 px-8">
-            <img src={darkSwitch} alt="switch" className=" cursor-pointer" />
+          <div
+            onClick={toggleSwitch}
+            className="flex justify-center items-center md:pb-8 px-8"
+          >
+            <img
+              src={!switchToggler ? darkSwitch : lightSwitch}
+              alt="switch"
+              className=" cursor-pointer"
+            />
           </div>
           <div className=" group flex justify-center items-center md:py-6 md:border-t  border-r-0 border-[#494E6E] px-8 border-l md:relative">
             <img src={logOut} alt="logOut" className=" h-6 cursor-pointer" />
