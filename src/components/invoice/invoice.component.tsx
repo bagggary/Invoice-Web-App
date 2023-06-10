06a23de2f70d61090cc63a1d5280d49f1f4ffb51
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-10 21:57:44 +0300

    invoices display styles

--- src/components/invoice/invoice.component.tsx
+++ src/components/invoice/invoice.component.tsx
@@ -25,24 +25,24 @@ const Invoice: React.FC<InvoiceProps> = ({ data }) => {
   return (
     <Link
       to={`/home/${id}`}
-      className=" border dark:bg-blue-dark hover:border-solid hover:shadow-none border-transparent duration-200 ease-linear hover:border-primary flex  justify-between items-center pl-8 pr-6 py-6 rounded-lg cursor-pointer"
+      className=" border dark:bg-blue-dark hover:border-solid hover:shadow-none border-transparent duration-200 ease-linear hover:border-primary flex flex-col sm:flex-row justify-between sm:items-center pl-8 pr-6 py-6 rounded-lg cursor-pointer"
     >
-      <div className="flex justify-between items-center mr-[2em]">
-        <div className=" text-sm font-bold mr-[1.75rem] dark:text-white text-black-1 ">
+      <div className="flex justify-between items-center sm:mr-[2em]">
+        <div className=" text-sm font-bold sm:mr-[1.75rem] dark:text-white text-black-1 ">
           <span className=" text-torko">#</span>
           {id}
         </div>
-        <div className="text-sm text-torko font-medium text-center ml-auto dark:text-gray-light">
+        <div className="text-sm text-torko font-medium text-center sm:ml-auto dark:text-gray-light">
           <span className=" text-dark-gray dark:text-gray-light">Due</span>{" "}
           {formatedDate}
         </div>
       </div>
-      <div className="flex items-center w-[60%] ">
-        <div className="flex justify-between items-center w-full">
+      <div className="flex items-center sm:w-[60%] mt-6 sm:mt-0 ">
+        <div className="flex sm:flex-row flex-col justify-between sm:items-center w-full">
           <p className="text-sm inline-block font-medium text-[#858BB2] dark:text-white">
             {clientName}
           </p>
-          <div className="font-bold text-black-1 text-lg dark:text-white">
+          <div className="font-bold text-black-1 text-base sm:text-lg dark:text-white">
             £{" "}
             {total.toLocaleString("en-US", {
               minimumFractionDigits: 2,
@@ -50,9 +50,13 @@ const Invoice: React.FC<InvoiceProps> = ({ data }) => {
             })}
           </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex self-end items-center">
           <Status status={status} />
-          <img src={rightarrow} alt="rightarrow" className=" ml-5" />
+          <img
+            src={rightarrow}
+            alt="rightarrow"
+            className=" ml-5 sm:block hidden"
+          />
         </div>
       </div>
     </Link>
