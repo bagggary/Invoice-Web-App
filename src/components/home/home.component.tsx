07e271eab83763ddd2fde4b98a68d6ba76d79de9
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-12 22:18:08 +0300

    modify container styles

--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,11 +1,13 @@
+import NewForm from "../forms/NewForm.component";
 import Header from "../header/header.component";
 import Invoices from "../invoices/invoices.component";
 
 const Home = () => {
   return (
-    <main className="md:mt-[72px]  container md:w-[730px] w-[672px] mt-14 mx-auto">
+    <main className="w-[90%] min-h-screen  pb-14 pt-[9rem] md:pt-[72px] md:pl-[5rem] lg:pl-0 md:w-[730px]  py-14  mx-auto  ">
       <Header />
       <Invoices />
+      <NewForm />
     </main>
   );
 };
