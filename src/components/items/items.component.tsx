c3cc31d699b1779aab8ae3aeb0260d6e4b7ac281
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-15 23:48:43 +0300

    form invoice items details , ablitiy to add and delelte new items

--- src/components/items/items.component.tsx
+++ src/components/items/items.component.tsx
@@ -1,20 +1,28 @@
 const Items = ({ item }: any) => {
   return (
     <div className="flex  justify-between ">
-      <div className=" w-[60%] font-bold text-sm text-black-1 dark:text-white">
-        {item.name}
+      <div className=" w-[60%] font-bold text-sm text-black-1  dark:text-white">
+        <div className="font-bold text-sm text-black-1 dark:text-white">
+          {item.name}
+        </div>
+        <div className="font-bold sm:hidden text-sm mt-2 text-torko dark:text-dark-gray">{`${
+          item.quantity
+        } X £${item.price.toLocaleString("en-US", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}`}</div>
       </div>
-      <div className="text-left font-bold text-sm text-black-1 dark:text-white">
+      <div className="text-left hidden sm:block font-bold text-sm text-black-1 dark:text-white">
         {item.quantity}
       </div>
-      <div className="font-bold text-sm text-black-1 dark:text-white">
+      <div className="font-bold hidden sm:block text-sm text-black-1 dark:text-white">
         £
         {item.price.toLocaleString("en-US", {
           minimumFractionDigits: 2,
           maximumFractionDigits: 2,
         })}
       </div>
-      <div className="font-bold text-sm text-black-1 dark:text-white">
+      <div className="font-bold text-sm text-black-1 self-center sm: dark:text-white">
         £
         {item.total.toLocaleString("en-US", {
           minimumFractionDigits: 2,
