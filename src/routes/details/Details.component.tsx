e3fd9f8fffe3b2d2329c709f416957713bd711b9
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-17 23:55:40 +0300

    modified responsivness styles for detailed invoices

--- src/routes/details/Details.component.tsx
+++ src/routes/details/Details.component.tsx
@@ -1,7 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import backArrow from "../../assets/icon-arrow-left.svg";
 import Status from "../../components/status/status.component";
-import Button from "../../components/Button.component";
+import Button from "../../components/button/Button.component";
 import { useSelector } from "react-redux";
 import { selectInvoicesData } from "../../store/invoice/invoice.selector";
 import Items from "../../components/items/items.component";
@@ -28,10 +28,8 @@ const Details = () => {
   } = invoiceData[0];
   const { city, country, postCode, street } = senderAddress;
 
-  console.log(items);
-
   return (
-    <div className="md:pt-[72px] w-[90%]  md:pr-[5rem] md:min-w-[730px] md:pl-[5rem] lg:pl-0 md:w-[730px] py-14  mx-auto">
+    <div className="md:pt-[72px] w-[90%] pt-[9rem]  md:pr-[5rem] md:min-w-[730px] md:pl-[5rem] lg:pl-0 md:w-[730px] py-14  mx-auto">
       <Link to=".." className="flex items-center gap-6 cursor-pointer ">
         <img src={backArrow} alt="back arrow" />
         <p className="font-bold text-sm hover:text-torko dark:text-white">
@@ -44,13 +42,13 @@ const Details = () => {
       {/*  */}
       {/*  */}
       <div className="w-full bg-white dark:bg-blue-dark h-[88px] mt-8 rounded-lg flex justify-between px-8">
-        <div className="flex  items-center">
+        <div className="flex  items-center w-full sm:w-auto">
           <p className=" font-medium text-sm text-[#858BB2] dark:text-gray-light">
             Status
           </p>
           <Status status={status} />
         </div>
-        <div className=" fixed bottom-0 bg-red-light sm:bg-transparent  sm:static sm: flex items-center">
+        <div className=" fixed bottom-0  sm:bg-transparent w-full h-[5rem] left-0 justify-center bg-white dark:bg-blue-dark sm:w-auto  sm:static flex items-center">
           <div className="flex items-center gap-2">
             <Button text="Edit" type="secondry" />
             <Button text="Delete" type="danger" />
@@ -64,7 +62,7 @@ const Details = () => {
       {/*  */}
       {/*  */}
       <div className="w-full bg-white dark:bg-blue-dark mt-6 rounded-lg p-12">
-        <div className="flex justify-between ">
+        <div className="flex sm: justify-between gap-[30px] sm:gap-0 sm:flex-row flex-col ">
           <div className="flex gap-2 flex-col">
             <p className="font-bold text-lg text-black-1 dark:text-white">
               <span className=" text-dark-gray">#</span>
@@ -75,7 +73,7 @@ const Details = () => {
             </p>
           </div>
 
-          <div className="flex flex-col gap-[2px] text-right">
+          <div className="flex flex-col gap-[2px] sm:text-right text-left">
             <p className="font-medium text-xs text-torko dark:text-gray-light">
               {street}
             </p>
@@ -95,9 +93,9 @@ const Details = () => {
         {/*  */}
         {/*  */}
         {/*  */}
-        <div className="flex items-start justify-between mt-6">
+        <div className="flex items-start sm:flex-row flex-col gap-8 sm:gap-0  sm:justify-between mt-6">
           {/* this container contain two group  */}
-          <div className="flex items-center justify-between w-[60%]">
+          <div className="flex items-center justify-between w-full sm:w-[60%]">
             {/* group one  */}
             <div className="flex flex-col gap-5">
               <div className="flex flex-col gap-3">
@@ -155,7 +153,7 @@ const Details = () => {
         {/* invoices data table continaer */}
         {/* data discription rows and columns */}
         <div className=" p-8 bg-white dark:bg-blue-light rounded-t-lg mt-8">
-          <div className="flex items-center justify-between ">
+          <div className="sm:flex items-center justify-between hidden">
             <div className="w-[50%] font-medium text-xs text-torko dark:text-gray-light">
               Item Name
             </div>
@@ -169,7 +167,7 @@ const Details = () => {
               Total
             </div>
           </div>
-          <div className="mt-8 flex flex-col gap-8">
+          <div className="sm:mt-8 flex flex-col gap-8">
             {items.map((item: any, index: any) => {
               return <Items item={item} key={index} />;
             })}
@@ -177,7 +175,7 @@ const Details = () => {
         </div>
         <div className="flex items-center justify-between p-8 bg-draft text-white  dark:bg-black-1 rounded-b-lg">
           <div className="font-medium text-sm">Amount Due</div>
-          <div className="font-bold text-2xl">
+          <div className="font-bold sm:text-2xl text-xl">
             £
             {total.toLocaleString("en-US", {
               minimumFractionDigits: 2,
