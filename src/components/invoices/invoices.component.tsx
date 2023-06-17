3238d3e3717a5a9c155ffd989694f6bb16ba3a3c
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-17 23:56:20 +0300

    fitered data by searchParams value

--- src/components/invoices/invoices.component.tsx
+++ src/components/invoices/invoices.component.tsx
@@ -1,13 +1,21 @@
 import { useSelector } from "react-redux";
 import Invoice from "../invoice/invoice.component";
 import { selectInvoicesData } from "../../store/invoice/invoice.selector";
+import { useSearchParams } from "react-router-dom";
 
 export default function Invoices() {
   const { Data } = useSelector(selectInvoicesData);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const typedFilter = searchParams.get("status");
+  const statusFilteredData = typedFilter
+    ? Data.filter((stat) => stat.status === typedFilter?.toLowerCase())
+    : Data;
+  console.log(statusFilteredData);
 
   return (
     <div className="mt-16 flex flex-col gap-4">
-      {Data?.map((invoicesData) => (
+      {statusFilteredData?.map((invoicesData) => (
         <Invoice key={invoicesData.id} data={invoicesData} />
       ))}
     </div>
