03e2b8ebea04aae8a642c860e7c8f2af9da6ee70
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-06-17 23:51:37 +0300

    modified ts svgr() config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class", '[data-theme="dark"]'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
   theme: {
     extend: {
+      content: {
+        link: "url(./src/assets/icon-check.svg)",
+      },
       colors: {
         primary: "#7c5dfa",
         secondry: "#9277ff",
         red: "#EC5757",
         "red-light": "#FF9797",
         "gray-light": "#DFE3FA",
-        "gray-dark": "#888EBO",
+        "dark-gray": "#888EB0",
         "blue-dark": "#1E2139",
         "blue-light": "#252945",
         "light-BG": "#F8f8fb",
         black: "#141625",
         "black-1": "#0C0E16",
         torko: "#7E88C3",
+        pending: "#FF8F00",
+        paid: "#33D69F",
+        draft: "#373B53",
       },
     },
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
