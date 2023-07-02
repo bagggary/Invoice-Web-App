72faa0a58af740243cc818241e5b9cfcda643d9d
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-07-02 21:04:17 +0300

    modified styles

--- src/components/dropdown/dropdown.component.tsx
+++ src/components/dropdown/dropdown.component.tsx
@@ -16,15 +16,17 @@ const Dropdown = () => {
 
   return (
     <div className="flex gap-[10px] flex-col">
-      <label htmlFor="dropdown" className="text-torko font-medium text-sm ">
+      <label htmlFor="dropdown" className="text-torko   font-medium text-sm ">
         Payments Terms
       </label>
       <div
         id="dropdown"
-        className=" relative cursor-pointer h-12 w-[240px] border group: border-gray-light flex justify-between items-center p-6"
+        className=" relative cursor-pointer h-12 w-[240px] border bg-white dark:bg-blue-dark rounded-[4px] border-gray-light dark:border-blue-light flex justify-between items-center p-6"
         onClick={dropDownHandler}
       >
-        <p className="font-bold text-sm text-black-1">Net 30 Days</p>
+        <p className="font-bold text-sm dark:text-white text-black-1">
+          Net 30 Days
+        </p>
         <ArrowDown
           className={`${
             show ? "rotate-180" : "rotate-0"
@@ -35,11 +37,11 @@ const Dropdown = () => {
             show
               ? "opacity-100 translate-y-4 visible"
               : "opacity-0 translate-y-0 hidden"
-          } opacity-0  top-[3.5rem] left-0  bg-red-white w-[240px] rounded-lg transition-all duration-300 ease-linear`}
+          } opacity-0  top-[2.5rem] left-0 bg-white dark:bg-blue-light  bg-red-white w-[240px] rounded-lg transition-all duration-300 ease-linear`}
         >
           {options?.map((term) => {
             return (
-              <div className=" cursor-pointer hover:text-primary text-sm font-bold text-black-1 border-b border-gray-light px-6 py-3 last:border-0">
+              <div className=" cursor-pointer dark:gray-light dark:text-gray-light dark:hover:text-primary dark:border-blue-dark hover:text-primary text-sm font-bold text-black-1 border-b border-gray-light px-6 py-3 last:border-0">
                 {term.label}
               </div>
             );
