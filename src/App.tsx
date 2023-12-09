2afd479485eeab7e9c62dcc93f32cbe4cc76f14e
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-09 19:29:12 +0300

    resolve listener - update newData if user exsist

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Authentication from "./routes/Authentication/Authentication.component";
 import "./index.css";
 import {
   createDocumentFromUserAuth,
+  db,
+  invoicesListener,
   onAuthStateChangedListener,
 } from "./util/firebase.util";
 import { useDispatch } from "react-redux";
@@ -11,20 +13,77 @@ import { Route, Routes } from "react-router-dom";
 import Main from "./routes/Main/Main.component";
 import Sidebar from "./components/sidebar/sidebar.component";
 import Details from "./routes/details/Details.component";
+import { ref, onValue } from "firebase/database";
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
+    const handleUserAuthentication = async (user) => {
       if (user) {
-        createDocumentFromUserAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-    return unsubscribe;
-  }, []);
+        await createDocumentFromUserAuth(user);
+        const userDocRef = ref(db, `user/${user.uid}`);
 
-  
+        const unsubscribe = onValue(userDocRef, (snapshot) => {
+          const data = snapshot.val();
+          // If the user has data, start listening for updates
+          invoicesListener(user.uid);
+        });
+        return unsubscribe;
+
+        dispatch(setCurrentUser(user));
+      }
+    };
+
+    const unsubscribe = onAuthStateChangedListener(handleUserAuthentication);
+
+    return () => {
+      // Unsubscribe when the component unmounts
+      unsubscribe();
+    };
+  }, [dispatch]);
+  //   useEffect(() => {
+  //     const handleUserAuthentication = async (user) => {
+  //       if (user) {
+  //         await createDocumentFromUserAuth(user);
+  //         const userDocRef = ref(db, `user/${user.uid}`);
+  //         // const snapshot = await get(userDocRef);
+  //        on(userDocRef , 'value' , (snapshot) => [
+  //         const data = snapshot.val();
+  //         invoicesListener(user.uid)
+  //         ])
+
+  //         if (snapshot.exists()) {
+  //           // If the user has data, start listening for updates
+  //           invoicesListener(user.uid);
+  //         } else {
+  //           // Handle the case when a new value is created
+  //           console.log("New value created:", snapshot.val());
+  //           // Perform actions specific to new values
+  //         }
+
+  //         dispatch(setCurrentUser(user));
+  //       }
+  //     };
+  // //     const userDocRef = ref(db, `user/${user.uid}`);
+
+  // //     const unsubscribe = on(userDocRef, 'value', (snapshot) => {
+  // //       const data = snapshot.val();
+  // //       // If the user has data, start listening for updates
+  // //       invoicesListener(user.uid);
+  // //     });
+
+  // //     dispatch(setCurrentUser(user));
+  // //   }
+  // // };
+
+  //     const unsubscribe = onAuthStateChangedListener(handleUserAuthentication);
+
+  //     return () => {
+  //       // Unsubscribe when the component unmounts
+  //       unsubscribe();
+  //     };
+  //   }, [dispatch]);
+
   return (
     <div className=" bg-[#F8F8FB] dark:bg-black  ">
       <Routes>
