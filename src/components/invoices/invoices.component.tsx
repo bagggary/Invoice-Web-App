42ae3df0061c8b3188074f096e4498fb02ca5c1f
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-09 19:28:43 +0300

    resolve listener issue

--- src/components/invoices/invoices.component.tsx
+++ src/components/invoices/invoices.component.tsx
@@ -4,14 +4,16 @@ import { selectInvoicesData } from "../../store/invoice/invoice.selector";
 import { useSearchParams } from "react-router-dom";
 
 export default function Invoices() {
-  const { Data } = useSelector(selectInvoicesData);
+  const invoicesData = useSelector(selectInvoicesData);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const typedFilter = searchParams.get("status");
   const statusFilteredData = typedFilter
-    ? Data.filter((stat) => stat.status === typedFilter?.toLowerCase())
-    : Data;
-  console.log(statusFilteredData);
+    ? invoicesData &&
+      invoicesData.Data.filter(
+        (stat) => stat.status === typedFilter?.toLowerCase()
+      )
+    : invoicesData && invoicesData.Data;
 
   return (
     <div className="mt-16 flex flex-col gap-4">
