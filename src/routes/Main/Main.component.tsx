6027f3cda5a74ea6689075fff196e43a36f06a6a
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-15 00:16:31 +0300

    error updating user's invoices when adding new invoice

--- src/routes/Main/Main.component.tsx
+++ src/routes/Main/Main.component.tsx
@@ -5,14 +5,21 @@ import { useNavigate } from "react-router-dom";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { useEffect } from "react";
 import Home from "../../components/home/home.component";
-import { db } from "../../util/firebase.util";
+import {
+  db,
+  formatDataToDatabase,
+  writeDataToDatabase,
+} from "../../util/firebase.util";
 import { onValue, ref } from "firebase/database";
 import { fetchInvoicesSuccess } from "../../store/invoice/invoice.action";
+import { FormValues } from "../../components/types/types";
+import { set } from "date-fns";
 
 const Main = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector(selectCurrentUser);
+
   // const signOutHandler = async () => {
   //   await signOutUser();
   //   dispatch(clearInvoices());
@@ -25,9 +32,21 @@ const Main = () => {
     }
     const unsubscribe = () => {
       const userRef = ref(db, `user/${user.uid}`);
+      let updatedData: FormValues[] = [];
+      // let reconstructedDatabaseArray: FormValues[] = [];
       const unsubscribe = onValue(userRef, (snapshot) => {
-        const data = snapshot.val();
-        dispatch(fetchInvoicesSuccess(data));
+        const { Data } = snapshot.val();
+        if (Array.isArray(Data)) {
+          updatedData = Data;
+        } else if (typeof Data === "object") {
+          Object.keys(Data).forEach((key) => {
+            updatedData.push(Data[key]);
+            // reconstructedDatabaseArray.push(Data[key]);
+          });
+        }
+        dispatch(fetchInvoicesSuccess(updatedData));
+        // if (!reconstructedDatabaseArray) return;
+        // formatDataToDatabase(reconstructedDatabaseArray, user.uid);
       });
       return unsubscribe;
     };
