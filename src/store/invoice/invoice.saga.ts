d95620f4f18c6625802cc10829812a5a52595282
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-19 19:54:22 +0300

    firebase convert array data to object issue

--- src/store/invoice/invoice.saga.ts
+++ src/store/invoice/invoice.saga.ts
@@ -1,13 +1,12 @@
 import { call, put, takeLatest, all } from "redux-saga/effects";
 import { INVOICES_ACTION_TYPES } from "./invoice.types";
-import { db, getInvoicesAndDocument } from "../../util/firebase.util";
+import { getInvoicesAndDocument } from "../../util/firebase.util";
 import { fetchInvoicesFailed, fetchInvoicesSuccess } from "./invoice.action";
-import { useDispatch } from "react-redux";
-import { onValue, ref } from "firebase/database";
 
 export function* fetchInvoicesAsync() {
   try {
     const invoicesArray = yield call(getInvoicesAndDocument);
+    console.log(invoicesArray);
     yield put(fetchInvoicesSuccess(invoicesArray));
   } catch (error: Error | any) {
     console.log(error);
