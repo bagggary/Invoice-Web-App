59f64d085ee462e594d7067badae2c5da9fa5c8d
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-27 02:53:51 +0300

    empty invoices illustration

--- src/components/invoices/invoices.component.tsx
+++ src/components/invoices/invoices.component.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import Invoice from "../invoice/invoice.component";
 import { selectInvoicesData } from "../../store/invoice/invoice.selector";
 import { useSearchParams } from "react-router-dom";
+import emtpyInvoice from "../../assets/illustration-empty.svg";
 
 export default function Invoices() {
   const invoicesData = useSelector(selectInvoicesData);
@@ -15,12 +16,32 @@ export default function Invoices() {
         )
       : invoicesData
     : [];
-  return (
-    <div className="mt-16 flex flex-col gap-4">
-      {statusFilteredData &&
-        statusFilteredData?.map((invoicesData) => (
-          <Invoice key={invoicesData.id} data={invoicesData} />
-        ))}
-    </div>
-  );
+
+  if (invoicesData.length > 0) {
+    return (
+      <div className="mt-16 flex flex-col gap-4">
+        {statusFilteredData &&
+          statusFilteredData?.map((invoicesData) => (
+            <Invoice key={invoicesData.id} data={invoicesData} />
+          ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className=" absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] max-w-[16rem] p-4">
+        <img
+          src={emtpyInvoice}
+          alt="emptyInvoicesIllustration"
+          className=" mb-8"
+        />
+        <h4 className=" mb-4 text-xl font-bold text-center text-black-1 dark:text-white tracking-[-0.625px]">
+          There is nothing here
+        </h4>
+        <p className=" text-center font-medium text-sm leading-[15px] text-dark-gray dark:text-gray-light tracking-[-0.25px]">
+          Create an invoice by clicking the
+          <span className="font-bold"> New Invoice</span> button and get started
+        </p>
+      </div>
+    );
+  }
 }
