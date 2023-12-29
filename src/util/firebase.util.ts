f07d5b4c8c9ae885a0263ba25119ee7cd6d5c887
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-30 00:03:52 +0300

    multiple function to handle update data and get the key from the database

--- src/util/firebase.util.ts
+++ src/util/firebase.util.ts
@@ -14,6 +14,7 @@ import {
   ref,
   remove,
   set,
+  update,
 } from "firebase/database";
 import Data from "../assets/Data.json";
 
@@ -37,12 +38,12 @@ export const db = getDatabase();
 
 export const createDocumentFromUserAuth = async (user: any) => {
   if (!user) return;
-  const { uid } = user;
+  const { uid, email } = user;
   const userDocRef = ref(db, `user/${uid}`);
   try {
     const snapshot = await get(userDocRef);
     if (!snapshot.exists()) {
-      await set(userDocRef, { Data });
+      await set(userDocRef, { email, Data });
     }
   } catch (error) {
     console.error("user Created Error ", error);
@@ -70,7 +71,6 @@ export const getInvoicesAndDocument = async (): Promise<
     onValue(
       userRef,
       (snapshot) => {
-        // resolve(snapshot.val());
         const { Data } = snapshot.val();
         if (Array.isArray(Data)) {
           reconstructureData = Data;
@@ -88,20 +88,34 @@ export const getInvoicesAndDocument = async (): Promise<
   });
 };
 
+export const dataKey = async (id: string) => {
+  const { currentUser } = store.getState().user;
+  const { uid } = currentUser;
+  const userRef = ref(db, `user/${uid}`);
+  const snapshot = await get(userRef);
+  const { Data } = snapshot.val();
+  let dataKey: string | undefined;
+  Object.entries(Data).filter(([key, value]: any) => {
+    if (value.id === id) {
+      dataKey = key;
+    }
+  });
+  return dataKey;
+};
+
+export const updateData = async (id: string, userId: string, dataToUpdate) => {
+  try {
+    const key = await dataKey(id);
+    await update(ref(db, `user/${userId}/Data/${key}`), dataToUpdate);
+  } catch (e) {
+    console.log("error : ", e);
+  }
+};
+
 export const deleteFromDatabase = async (id: string, userId: string) => {
   try {
-    const { currentUser } = store.getState().user;
-    const { uid } = currentUser;
-    const userRef = ref(db, `user/${uid}`);
-    const snapshot = await get(userRef);
-    const { Data } = snapshot.val();
-    let dataKey: string | undefined;
-    Object.entries(Data).filter(([key, value]: any) => {
-      if (value.id === id) {
-        dataKey = key;
-      }
-    });
-    remove(ref(db, `user/${userId}/Data/${dataKey}`));
+    const key = await dataKey(id);
+    remove(ref(db, `user/${userId}/Data/${key}`));
   } catch (e) {
     console.log("error :", e);
   }
