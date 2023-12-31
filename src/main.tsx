25f9f337f87d525d7f5b1ba84b2817ef9ab20f90
ENG-SKYLER <ibraheema.a2014@gmail.com>
2023-12-31 18:05:39 +0300

    edit styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import { store, persistor } from "./store/store.ts";
 import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
