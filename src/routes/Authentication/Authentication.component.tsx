c268f52e6eb2550c59e98a50e6cd777a24c342ae
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-01 23:04:37 +0300

    signout function when rendreing the authentication page

--- src/routes/Authentication/Authentication.component.tsx
+++ src/routes/Authentication/Authentication.component.tsx
@@ -1,7 +1,24 @@
 import SignUp from "../../components/SignUp/Signup.component";
 import SignIn from "../../components/SignIn/SignIn.component";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { signOutUser } from "../../util/firebase.util";
+import { clearInvoices } from "../../store/invoice/invoice.action";
 
 const Authentication = () => {
+  const dispatch = useDispatch();
+
+  const signOutHandler = async () => {
+    await signOutUser();
+    dispatch(clearInvoices());
+  };
+
+  useEffect(() => {
+    signOutHandler();
+  }, []);
+
+  console.log("render from authentication component");
+
   return (
     <>
       <div className="flex w-full ">
