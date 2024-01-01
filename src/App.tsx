5b5f5807497a0e3762dfeb22a467ab3a0f2fe4dd
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-01 23:05:05 +0300

    added data listener

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import Authentication from "./routes/Authentication/Authentication.component";
 import "./index.css";
 import {
   createDocumentFromUserAuth,
+  db,
   onAuthStateChangedListener,
 } from "./util/firebase.util";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setCurrentUser } from "./store/user/user.action";
 import { Route, Routes } from "react-router-dom";
 import Main from "./routes/Main/Main.component";
 import Sidebar from "./components/sidebar/sidebar.component";
 import Details from "./routes/details/Details.component";
+import PrivateRoute from "./routes/Private/privateRoute";
+import { onValue, ref } from "firebase/database";
+import { selectCurrentUser } from "./store/user/user.selector";
+import { FormValues } from "./components/types/types";
+import { fetchInvoicesSuccess } from "./store/invoice/invoice.action";
 
 function App() {
   const dispatch = useDispatch();
@@ -28,58 +34,59 @@ function App() {
     };
   }, []);
 
-  //   useEffect(() => {
-  //     const handleUserAuthentication = async (user) => {
-  //       if (user) {
-  //         await createDocumentFromUserAuth(user);
-  //         const userDocRef = ref(db, `user/${user.uid}`);
-  //         // const snapshot = await get(userDocRef);
-  //        on(userDocRef , 'value' , (snapshot) => [
-  //         const data = snapshot.val();
-  //         invoicesListener(user.uid)
-  //         ])
+  const user = useSelector(selectCurrentUser);
 
-  //         if (snapshot.exists()) {
-  //           // If the user has data, start listening for updates
-  //           invoicesListener(user.uid);
-  //         } else {
-  //           // Handle the case when a new value is created
-  //           console.log("New value created:", snapshot.val());
-  //           // Perform actions specific to new values
-  //         }
+  useEffect(() => {
+    const unsubscribe = () => {
+      const userRef = ref(db, `user/${user && user.uid}`);
+      let updatedData: FormValues[] = [];
+      let filteredData: FormValues[] | [];
+      const unsubscribe = onValue(userRef, (snapshot) => {
+        if (!snapshot.exists()) {
+          return;
+        }
+        const { Data } = snapshot.val();
+        if (Data) {
+          if (Array.isArray(Data)) {
+            updatedData = Data;
+          } else if (typeof Data === "object") {
+            updatedData = Object.values(Data);
+          }
+          filteredData = updatedData.filter((p) => p);
+          dispatch(fetchInvoicesSuccess(filteredData));
+        } else {
+          dispatch(fetchInvoicesSuccess([]));
+        }
+      });
+      return unsubscribe;
+    };
 
-  //         dispatch(setCurrentUser(user));
-  //       }
-  //     };
-  // //     const userDocRef = ref(db, `user/${user.uid}`);
-
-  // //     const unsubscribe = on(userDocRef, 'value', (snapshot) => {
-  // //       const data = snapshot.val();
-  // //       // If the user has data, start listening for updates
-  // //       invoicesListener(user.uid);
-  // //     });
-
-  // //     dispatch(setCurrentUser(user));
-  // //   }
-  // // };
-
-  //     const unsubscribe = onAuthStateChangedListener(handleUserAuthentication);
-
-  //     return () => {
-  //       // Unsubscribe when the component unmounts
-  //       unsubscribe();
-  //     };
-  //   }, [dispatch]);
-
-  console.log("render from main component ");
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <div className=" bg-[#F8F8FB] dark:bg-black  ">
       <Routes>
         <Route path="/" element={<Authentication />} />
-        <Route path="/home" element={<Sidebar />}>
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Sidebar />
+            </PrivateRoute>
+          }
+        >
           <Route index element={<Main />} />
-          <Route path=":Id" element={<Details />} />
+          <Route
+            path=":Id"
+            element={
+              <PrivateRoute>
+                <Details />
+              </PrivateRoute>
+            }
+          />
         </Route>
       </Routes>
     </div>
