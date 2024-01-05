56112a245bd4a22c3da9df343223eea2c8a4c1c8
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-05 19:21:24 +0300

    title change

--- src/routes/Main/Main.component.tsx
+++ src/routes/Main/Main.component.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Home from "../../components/home/home.component";
 
 const Main = () => {
@@ -6,6 +7,10 @@ const Main = () => {
   //   dispatch(clearInvoices());
   //   navigate("/");
   // };
+
+  useEffect(() => {
+    document.title = "Home | Invoice App";
+  }, []);
 
   return <Home />;
 };
