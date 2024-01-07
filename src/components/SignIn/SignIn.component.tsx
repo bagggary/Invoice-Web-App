c37ce60f683162a990f58afd81727677e74b2a5f
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-01-07 22:46:36 +0300

    change structure of both login and sign up page

--- src/components/SignIn/SignIn.component.tsx
+++ src/components/SignIn/SignIn.component.tsx
@@ -1,14 +1,20 @@
-// import Logo from '../../assets/logo.svg'
 import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Auth, createDocumentFromUserAuth } from "../../util/firebase.util";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "../../store/user/user.action";
-// import { onValue, ref } from "firebase/database";
 import { fetchInvoicesStart } from "../../store/invoice/invoice.action";
 import { useNavigate } from "react-router-dom";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 
-const SignIn = () => {
+const SignIn = ({
+  status,
+  setStatus,
+}: {
+  status: boolean;
+  setStatus: (status: boolean) => void;
+}) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   type SignInTypes = {
@@ -51,93 +57,123 @@ const SignIn = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a
-        href="#"
-        className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-      >
-        <img
-          className="w-8 h-8 mr-2"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-          alt="logo"
+    <form
+      className={`space-y-4 md:space-y-6 transition-opacity duration-300 ease-in-out ${
+        status ? "opacity-100" : "opacity-0"
+      }`}
+    >
+      <div className="relative flex items-center mt-8">
+        <MdOutlineEmail className="absolute w-6 h-6 mx-3 text-gray-300 dark:text-gray-500 " />
+        <input
+          type="email"
+          name="Email"
+          id="email"
+          className="block w-full  px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-primary focus:border-secondry dark:focus:border-secondry focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Email"
         />
-        Flowbite
-      </a>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Sign In
-          </h1>
-          <form
-            className="space-y-4 md:space-y-6"
-            action="#"
-            onSubmit={(e) => sumbitHandler(e)}
-          >
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Your email
-              </label>
-              <input
-                onChange={(e) => changeHandler(e)}
-                type="email"
-                name="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                onChange={(e) => changeHandler(e)}
-                type="password"
-                name="password"
-                id="password"
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Sign In
-            </button>
-          </form>
-        </div>
       </div>
-    </div>
+      <div className="relative flex items-center mt-4">
+        <RiLockPasswordFill className="absolute w-6 h-6 mx-3 text-gray-300 dark:text-gray-500 " />
+        <input
+          type="password"
+          name="password"
+          className="block w-full  px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-primary focus:border-secondry dark:focus:border-secondry focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Password"
+          id="password"
+        />
+      </div>
+
+      <div className="flex items-center justify-between">
+        <a
+          href="#"
+          className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 dark:text-gray-300"
+        >
+          Forgot password?
+        </a>
+      </div>
+      <button
+        type="button"
+        className="w-full text-white  bg-primary hover:bg-secondry   font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-secondry  cursor-pointer"
+      >
+        Sign in
+      </button>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        Don’t have an account yet?
+        <div
+          className="font-medium text-primary hover:underline dark:text-primary-500 cursor-pointer "
+          onClick={() => setStatus(!status)}
+        >
+          Sign up
+        </div>
+      </p>
+    </form>
   );
 };
 export default SignIn;
 
 {
-  /* <div>
-                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  />
-                    </div> */
-}
-{
-  /* <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required={true} />
-                        </div>
-                        <div className="ml-3 text-sm">
-                          <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                        </div>
-                    </div> */
-}
-
-{
-  /* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                    </p> */
+  /* <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+<a
+  href="#"
+  className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+>
+  <img
+    className="w-8 h-8 mr-2"
+    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+    alt="logo"
+  />
+  Flowbite
+</a>
+<div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+  <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+      Sign In
+    </h1>
+    <form
+      className="space-y-4 md:space-y-6"
+      action="#"
+      onSubmit={(e) => sumbitHandler(e)}
+    >
+      <div>
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your email
+        </label>
+        <input
+          onChange={(e) => changeHandler(e)}
+          type="email"
+          name="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@company.com"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Password
+        </label>
+        <input
+          onChange={(e) => changeHandler(e)}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="••••••••"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+      >
+        Sign In
+      </button>
+    </form>
+  </div>
+</div>
+</div> */
 }
--- src/components/SignUp/Signup.component.tsx
+++ src/components/SignUp/Signup.component.tsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Auth, createDocumentFromUserAuth } from "../../util/firebase.util";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
 
-const SignUp = () => {
+const SignUp = ({
+  status,
+  setStatus,
+}: {
+  status: boolean;
+  setStatus: (status: boolean) => void;
+}) => {
   type FormTypes = {
     email: string;
     password: string;
@@ -51,96 +59,55 @@ const SignUp = () => {
     resetFormFields();
   };
   return (
-    <div className="flex  flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <a
-        href="#"
-        className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-      >
-        <img
-          className="w-8 h-8 mr-2"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-          alt="logo"
+    <form
+      className={`w-full max-w-md duration-300 transition-opacity linear ${
+        !status ? "opacity-100" : "opacity-0 hidden "
+      }`}
+    >
+      <div className="relative flex items-center mt-8">
+        <MdOutlineEmail className="absolute w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" />
+        <input
+          type="email"
+          className="block w-full  px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-primary focus:border-secondry dark:focus:border-secondry focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Email"
         />
-        Flowbite
-      </a>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-            Create and account
-          </h1>
-          <form className="space-y-4 md:space-y-6" action="#">
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Your email
-              </label>
-              <input
-                onChange={(e) => changeHandler(e)}
-                type="email"
-                name="email"
-                id="email"
-                value={formFields.email}
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Password
-              </label>
-              <input
-                onChange={(e) => changeHandler(e)}
-                type="password"
-                name="password"
-                id="password"
-                value={formFields.password}
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="confirmPassword"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Confirm password
-              </label>
-              <input
-                onChange={(e) => changeHandler(e)}
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                value={formFields.confirmPassword}
-                placeholder="••••••••"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              />
-            </div>
-            {/* <div className="flex items-start">
-                      <div className="flex items-center h-5">
-                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required={true} />
-                      </div>
-                      <div className="ml-3 text-sm">
-                        <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                      </div>
-                  </div> */}
-            <button
-              onClick={(e) => submitHandler(e)}
-              className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Create an account
-            </button>
-            {/* <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                  </p> */}
-          </form>
+      </div>
+
+      <div className="relative flex items-center mt-4">
+        <RiLockPasswordFill className="absolute w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" />
+        <input
+          type="password"
+          className="block w-full  px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-primary focus:border-secondry dark:focus:border-secondry focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Password"
+          id="password"
+        />
+      </div>
+
+      <div className="relative flex items-center mt-4">
+        <RiLockPasswordFill className="absolute w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" />
+        <input
+          type="password"
+          className="block w-full  px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-primary focus:border-secondry dark:focus:border-secondry focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Confirm Password"
+          id="confirmpassword"
+        />
+      </div>
+
+      <div className="mt-6">
+        <button className="w-full text-white  bg-primary hover:bg-secondry   font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-secondry  cursor-pointer">
+          Sign Up
+        </button>
+
+        <div className="mt-6 text-center ">
+          <a
+            className="text-sm text-primary hover:underline dark:text-secondry cursor-pointer"
+            onClick={() => setStatus(!status)}
+          >
+            Already have an account?
+          </a>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
