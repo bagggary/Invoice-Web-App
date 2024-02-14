559cfb1a062600fa7748cc030a7a6452930a0ba9
ENG-SKYLER <ibraheema.a2014@gmail.com>
2024-02-15 02:31:33 +0300

    change .env to meta

--- src/util/firebase.util.ts
+++ src/util/firebase.util.ts
@@ -22,13 +22,13 @@ import { store } from "../store/store";
 import { FormValues } from "../components/types/types";
 
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey: import.meta.env.REACT_APP_API_KEY,
+  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_ID,
+  databaseURL: import.meta.env.REACT_APP_DATABASE_URL,
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
